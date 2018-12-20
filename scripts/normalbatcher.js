5bbd5d4e4d80ff410df48c75eced04abcafe2504
mariomui <yi.kan.mui@gmail.com>
2018-12-20 13:48:26 -0800

    Replaced doublequotes with singlequotes

--- scripts/normalbatcher.js
+++ scripts/normalbatcher.js
@@ -1,37 +1,37 @@
-const faker = require("faker");
-const mongoose = require("mongoose");
+const faker = require('faker');
+const mongoose = require('mongoose');
 // const Promise = require('bluebird');
 
 mongoose
   .connect(
-    "mongodb://localhost:27017/carousel",
+    'mongodb://localhost:27017/carousel',
     { useNewUrlParser: true },
     () => {
-      mongoose.connection.dropCollection("pictures", () => {
-        console.log("pictures dropped");
+      mongoose.connection.dropCollection('pictures', () => {
+        console.log('pictures dropped');
       });
-    }
+    },
   )
   .then(() => {
     var roomPicturesSchema = new mongoose.Schema(
       {
         name: {
-          type: String
+          type: String,
         },
         abstract: {
-          type: String
+          type: String,
         },
         image_url: {
-          type: [String]
+          type: [String],
         },
         room_id: {
-          type: Number
-        }
+          type: Number,
+        },
       },
-      { timestamps: true }
+      { timestamps: true },
     );
 
-    const pictureMaker = mongoose.model("picture", roomPicturesSchema);
+    const pictureMaker = mongoose.model('picture', roomPicturesSchema);
     var collections = [];
     for (let i = 0; i < 100; i++) {
       var temp = [];
@@ -43,12 +43,12 @@ mongoose
         name: faker.name.findName(),
         abstract: faker.image.abstract(),
         image_url: temp,
-        room_id: i
+        room_id: i,
       };
       collections.push(obj);
     }
     console.log(collections);
-    pictureMaker.create(collections).then(function() {
+    pictureMaker.create(collections).then(function () {
       mongoose.connection.close();
     });
   });
