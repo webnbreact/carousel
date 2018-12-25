ae2ce5a893f5ab0ee4470499724fded35673acab
mariomui <yi.kan.mui@gmail.com>
2018-12-25 14:06:51 -0800

    Created barebones Landing Component
    Landing succesfually obtains the correct array of pictures and updates
    State on mount.

--- client/components/CarouselLanding.jsx
+++ client/components/CarouselLanding.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 import React from 'react';
+import axios from 'axios';
+import url from 'url';
 
 class CarouselLanding extends React.Component {
   constructor() {
@@ -10,9 +12,37 @@ class CarouselLanding extends React.Component {
     };
   }
 
+
+  componentDidMount() {
+    const currentUrl = window.location.href;
+    const dbLocation = 'http://localhost:4500';
+    const pathName = url.parse(currentUrl).pathname;
+    const postUrl = url.resolve(dbLocation, pathName);
+    this.getDataFromServer(postUrl)
+      .then((data) => {
+        this.setState({ image_url: data });
+      });
+  }
+
+  getDataFromServer(postUrl) {
+    return axios.get(postUrl)
+      .then(response => (response.data));
+  }
+
   render() {
+    const { image_url } = this.state;
     return (
-      <div> Hot moduddle repalcement!! </div>
+      <div>
+        {' '}
+        {image_url.map(datum => (
+          <div>
+            {' '}
+            {datum}
+            {' '}
+          </div>
+        ))}
+        {' '}
+      </div>
     );
   }
 }
