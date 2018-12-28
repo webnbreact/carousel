be64407eecfcea4f71a721e7e067da0ac443b299
mariomui <yi.kan.mui@gmail.com>
2018-12-27 23:33:31 -0800

    Styled cards to mimic airbnb splash page

--- client/components/CarouselCard.jsx
+++ client/components/CarouselCard.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
+
+const Image = styled.img`
+  border: 1px solid black;
+  margin: 2px;
+  :nth-of-type(n+2) {
+    width: 21%;
+    height: 21%;
+  }
+  :first-of-type {
+    float: left;
+    padding: 0px
+  }
+`;
+const Wrapper = styled.div`
+  display: grid;
+`;
 const CarouselCard = ({ imageUrl, key }) => (
-  <img src={imageUrl} key={key} alt="pictures" />
+  // <Wrapper className="picture-row">
+  <Image className="media-element" src={imageUrl} key={key} alt="pictures" />
+  // </Wrapper>
 );
 
 export default CarouselCard;
