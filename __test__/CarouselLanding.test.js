f4fbbec5edbcae091e6c93e5b1434008679a1d1a
mariomui <yi.kan.mui@gmail.com>
2018-12-27 22:58:53 -0800

    Remove chai package

--- __test__/CarouselLanding.test.js
+++ __test__/CarouselLanding.test.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import Enzyme, { shallow, mount } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import { expect } from 'chai';
 import Adapter from 'enzyme-adapter-react-16';
-// import renderer from 'react-test-renderer';
-import { Z_TREES } from 'zlib';
-import CarouselCentral from '../client/CarouselCentral';
 import CarouselLanding from '../client/components/CarouselLanding';
 import CarouselCard from '../client/components/CarouselCard';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-// describe('hey', () => {
-
-// });
 describe('Carousel Landing', () => {
-  // const tree = mount(<CarouselLanding />);
-
+  it('to exist', () => {
+    const tree = shallow(<CarouselLanding />);
+    expect(toJson(tree)).toMatchSnapshot();
+  });
   it('to render five carousel card components', () => {
     const tree = shallow(<CarouselLanding />);
     tree.setState({
@@ -27,7 +22,7 @@ describe('Carousel Landing', () => {
         'http://google.com',
       ],
     });
-    expect(tree.find(CarouselCard)).to.have.lengthOf(4);
+    expect(tree.find(CarouselCard).length).toEqual(4);
   });
 });
 
