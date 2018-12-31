9692d8686c128ce2c4fcae6224c02f3a940d3689
mariomui <yi.kan.mui@gmail.com>
2018-12-30 17:32:37 -0800

    Implement Zoom-in on Cards

--- client/components/CarouselCard.jsx
+++ client/components/CarouselCard.jsx
@@ -3,24 +3,40 @@ import styled from 'styled-components';
 
 
 const Image = styled.img`
+
   border: 1px solid black;
-  margin: 2px;
-  :nth-of-type(n+2) {
-    width: 21%;
-    height: 21%;
-  }
-  :first-of-type {
-    float: left;
-    padding: 0px
-  }
+  background: no-repeat url(${({ imageUrl }) => imageUrl});
+  background-size: cover;
+  background-position: center;
+  min-width: 100%;
+  min-height: 100%;
+  :hover {
+  color: #424242; 
+  background-size:cover;
+
+  -webkit-transition: all .3s ease-out;
+  -moz-transition: all .3s ease-out;
+  -ms-transition: all .3s ease-out;
+  -o-transition: all .3s ease-out;
+  transition: all .3s ease-out;
+  opacity: 1;
+  transform: scale(1.3);
+  -ms-transform: scale(1.3); /* IE 9 */
+  -webkit-transform: scale(1.3); /* Safari and Chrome */
+  }  
 `;
-const Wrapper = styled.div`
-  display: grid;
+
+const Wrap = styled.span`
+  position: relative;
+  overflow: hidden;
+  border: 1px solid transparent;
+  
 `;
 const CarouselCard = ({ imageUrl, key }) => (
-  // <Wrapper className="picture-row">
-  <Image className="media-element" src={imageUrl} key={key} alt="pictures" />
-  // </Wrapper>
+  <Wrap>
+    <Image imageUrl={imageUrl} key={key} alt="pictures" />
+  </Wrap>
+
 );
 
 export default CarouselCard;
--- client/components/CarouselLanding.jsx
+++ client/components/CarouselLanding.jsx
@@ -3,7 +3,47 @@
 import React from 'react';
 import axios from 'axios';
 import url from 'url';
+// import { Grid } from '../css/CarouselLanding';
+import styled from 'styled-components';
 import CarouselCard from './CarouselCard';
+
+// const Wrapper = styled.div`
+
+//   img:first-of-type {
+//     max-width: 40%;
+//     max-height: 40%;
+//     overflow:hidden;
+//   }
+//   img:nth-of-type(n+2) {
+
+//     margin: 1px;
+
+//     max-height: 150px;
+//     max-width: 150px;
+//     overflow: hidden;
+//   }
+
+// `;
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto 350px 350px;
+  grid-auto-rows: 350px
+  gap: 0px;
+  column-gap: 0px;
+`;
+
+const OneLot = styled.div`
+  display: grid;
+  grid-column: 1/3;
+  gap: 0px;
+`;
+
+const FourLot = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  border:1px solid transparent;
+  overflow:hidden;
+`;
 
 class CarouselLanding extends React.Component {
   constructor() {
@@ -12,6 +52,7 @@ class CarouselLanding extends React.Component {
       imageUrls: [],
     };
   }
+
 
   componentDidMount() {
     const currentUrl = window.location.href;
@@ -33,11 +74,19 @@ class CarouselLanding extends React.Component {
     const { imageUrls } = this.state;
 
     return (
-      <div>
-        {imageUrls.map((imageUrl, key) => (
-          <CarouselCard key={key} imageUrl={imageUrl} />
-        ))}
-      </div>
+      <Wrapper>
+        <OneLot>
+          <CarouselCard imageUrl={imageUrls[0]} />
+        </OneLot>
+        <FourLot>
+          {
+            imageUrls.map((imageUrl, key) => (
+              <CarouselCard key={key} imageUrl={imageUrl} />
+            )).slice(1)
+          }
+        </FourLot>
+      </Wrapper>
+
     );
   }
 }
